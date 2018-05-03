8f29e3b6962459a11581c2285a806b56386781ac
Beenczy <36893426+Beenczy@users.noreply.github.com>
2018-05-03 15:04:50 +0200

    Update 11.py

--- 11.py
+++ 11.py
@@ -2,3 +2,10 @@ def Exercise11():
     for i in range(100,1000):
         for j in range(100,1000):
             x=i*j
+            if str(x) == str(x)[::-1]:
+                y = str(x)
+                a = i
+                b = j
+    return '{0} * {1} = {2}'.format(a, b, y)
+
+print(Exercise11())
